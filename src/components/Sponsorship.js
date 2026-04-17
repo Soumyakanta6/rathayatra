866b2598cf62c16584133a422c300b8306f2afef
Soumyakanta6 <63486371+Soumyakanta6@users.noreply.github.com>
2026-04-17 17:19:49 +0530

    Update Sponsorship.js

--- src/components/Sponsorship.js
+++ src/components/Sponsorship.js
@@ -295,8 +295,6 @@ Jai Jagannath! 🙏
               <span className="reason-icon">🤝</span>
               <h3>Networking</h3>
               <p>Connect with community leaders, business owners, and like-minded individuals</p>
-              <ul className="reason-points">
-                <li>VIP networking lounge access</li>
                 <li>Meet community influencers</li>
                 <li>Business collaboration opportunities</li>
                 <li>Special place to showcase your business</li>
